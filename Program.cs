75f83221e4d22e634e13c25c887578735639315e
shioko <ichisadashioko@gmail.com>
2023-05-25 03:25:31 +0000

    :tada: init project

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Windows.Forms;
+
+public class Program {
+  public static void copy_text_to_clipboard(string text) {
+    try {
+      Clipboard.SetText(text);
+    } catch (Exception ex) {
+      Console.WriteLine(ex);
+      Console.WriteLine(ex.Message);
+    }
+  }
+  public static void Main() {
+    long unix_ts = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+    string ts_s = unix_ts.ToString();
+    Console.WriteLine(ts_s);
+    copy_text_to_clipboard(ts_s);
+  }
+}
